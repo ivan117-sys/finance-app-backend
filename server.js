cd6092b3cb5bb20f5fa9da68276a5e7d00efc262
ivan117-sys <ivanmuskovic@gmail.com>
2023-03-30 17:24:32 +0200

    2

--- server.js
+++ server.js
@@ -29,9 +29,6 @@ app.use('/api/users', users);
 
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
-app.listen(
-  PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
-);
+app.listen(PORT);
